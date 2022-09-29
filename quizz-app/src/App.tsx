99008c3bba532fe2dc7fdf53f35f8e64ca8ec08d
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2022-09-29 17:46:45 +0100

    created an enum and async func for the API

--- quizz-app/src/App.tsx
+++ quizz-app/src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
       />
       <button className="next" onClick={nextQuestion}>
         Next Question
