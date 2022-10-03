9f26f3a981f5bbed3c0b76cdc185c953992cbd40
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2022-10-03 17:48:48 +0100

    added types for the questions and also for the question state

--- quizz-app/src/API.ts
+++ quizz-app/src/API.ts
@@ -1,3 +1,15 @@
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string; 
+}
+
+export type QuestionState = Question & { answers: string[] };
+
 export enum Difficulty {
     EASY = "easy",
     MEDIUM = "medium",
